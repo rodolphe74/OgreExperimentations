3dff1e5eb4f736fdf5a9037d3febf3e1a755fe9d
rodolphe74 <rcordenie@gmail.com>
2024-03-22 08:50:57 +0100

    vcpkg shortened path

--- sln.ps1
+++ sln.ps1
@@ -1,13 +1,15 @@
 # https://github.com/microsoft/vcpkg
 
-cmake . -DCMAKE_TOOLCHAIN_FILE=C:\Users\rodoc\HOME\developpement\visual\vcpkg\scripts\buildsystems\vcpkg.cmake
+cmake . -DCMAKE_TOOLCHAIN_FILE=C:\Users\rodoc\HOME\vcpkg\scripts\buildsystems\vcpkg.cmake
 mkdir -Force Debug
 mkdir -Force Release
 
 cp -Verbose -Force resources/msvcdebugcfg/plugins.cfg Debug
 cp -Verbose -Force resources/msvcdebugcfg/resources.cfg Debug
 cp -Verbose -Force resources/* Debug
+cp -Verbose -Force resources/Chessboard/ogre/*.mesh Debug
 
 cp -Verbose -Force resources/msvcreleasecfg/plugins.cfg Release
 cp -Verbose -Force resources/msvcreleasecfg/resources.cfg Release
 cp -Verbose -Force resources/* Release
+cp -Verbose -Force resources/Chessboard/ogre/*.mesh Release
